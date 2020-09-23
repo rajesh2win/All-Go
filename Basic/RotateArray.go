00f33a30bfb5897a6d46daa29937463941b5bebf
Rajesh Nallamothu <Rajesh.nallamothu@glu.com>
2020-09-23 06:19:21 +0530

    Rotate Array

---
+++ Basic/RotateArray.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	inputAry := []int{2, 3, 5, 7, 11, 13}
+	k := 4
+	rotateArray(inputAry, k)
+	rotatWithAuxilaryArray(inputAry, k)
+
+}
+
+func rotateArray(inputAry []int, k int) {
+	len := len(inputAry) - 1
+
+	for i := 0; i < k; i++ {
+		tmp := inputAry[len]
+		for p := len; p >= 0; p-- {
+			if p == 0 {
+				inputAry[p] = tmp
+			} else {
+				inputAry[p] = inputAry[p-1]
+			}
+		}
+
+	}
+	fmt.Println(inputAry)
+}
+
+func rotatWithAuxilaryArray(inputAry [] int , k int){
+	len :=len(inputAry)
+	auxArray := [6] int{}
+	for i:=0;i<len;i++{
+		if i+k<len{
+			auxArray[i+k]=inputAry[i]
+		}else{
+			auxArray[(i+k)%len]=inputAry[i]
+		}
+	}
+	fmt.Println(auxArray)
+}
